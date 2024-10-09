08812df145ad2be30d1a5dcb3073481dfb79c33c
Chase Walters <55497704+Memotexe@users.noreply.github.com>
2024-10-09 17:50:22 -0400

    For Loops!

---
+++ Week7/src/ForLoops.java
@@ -0,0 +1,11 @@
+public class ForLoops{
+    public static void main(String[] args) {
+        int counter = 0;
+        for(int i = 0;i<10;i++){
+            for(int j = 0;j<10;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
