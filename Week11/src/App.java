8d606a58a0188686afffe767b3f5923b66cf785b
Chase Walters <55497704+Memotexe@users.noreply.github.com>
2024-11-11 15:52:35 -0500

    Beginning Week 12!

--- Week11/src/App.java
+++ Week11/src/App.java
@@ -3,32 +3,46 @@ public class App {
     public static void main(String[] args) throws Exception {
         int x, y;
         x = 5;
-        y= -12;
-        File file = new File("totalSales.txt");
-        Boolean value = Comparison(x, y, file);
-        System.out.println("True or False, is X > Y? " + value);
+        y= 12;
+        // File file = new File("totalSales.txt");
+        // Boolean value = Comparison(x, y, file);
+        // System.out.println("True or False, is X > Y? " + value);
 
-        //VOID METHOD CALL
-        //Comparison(23,4);
+        // VOID METHOD CALL
+        Comparison(x,y);
+        Comparison(23, 5);
+        Comparison(1212,543534);
 
 
     }
 
-    public static Boolean Comparison(int x, int y, File file){
-        if(x > y){
-            return true;
-        }else{
-            return false;
-        }
-    }
 
-
-    // public static void Comparison(int x, int y){
-
+/**
+ * Paragraph
+ * 
+ * 
+ * 
+ * 
+ * @param x
+ * @param y
+ * @param file This takes the users file input
+ * @return Boolean True or False
+ */
+    // public static Boolean Comparison(int x, int y, File file){
     //     if(x > y){
-    //         System.out.println("TRUE");
+    //         return true;
     //     }else{
-    //         System.out.println("FALSE");
+    //         return false;
     //     }
     // }
+
+
+    public static void Comparison(int x, int y){
+
+        if(x > y){
+            System.out.println("TRUE");
+        }else{
+            System.out.println("FALSE");
+        }
+    }
 }
